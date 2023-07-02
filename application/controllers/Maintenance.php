7fa0b1a217d2f3bc187a9c559c8e9b8566e049f9
Sumit kachariya <114276572+Jalmit@users.noreply.github.com>
2023-07-02 12:26:47 +0530

    upload code all file

--- application/controllers/Maintenance.php
+++ application/controllers/Maintenance.php
@@ -18,7 +18,7 @@ class Maintenance extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/userguide3/general/urls.html
 	 */
-	public function __construct()
+	public function __construct() 
     { 
         parent::__construct(); 
 		$maintenance = maintenance();
